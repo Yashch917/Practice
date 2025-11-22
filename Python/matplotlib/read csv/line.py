cf924210bf18c269861fc503c255f74bdfa40514
Yash <yashch8105@gmail.com>
2025-11-22 12:47:16 +0530

    matplot

--- Python/matplotlib/read csv/line.py
+++ Python/matplotlib/read csv/line.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 x=np.array([3,8,1,10])
 y=np.array([1,6,4,2])
-plt.plot(x,linestyle='dotted',marker='o',color='red')
+plt.plot(x,y,linestyle='dotted',marker='o',color='red')
 plt.plot(y,linestyle='dashed',c='#4CAF50',marker='x',linewidth=10)
+
+plt.fill_between(x,y,alpha=0.5,color='lightblue')
+
 plt.show()
--- Python/matplotlib/read csv/subplt.py
+++ Python/matplotlib/read csv/subplt.py
@@ -1,28 +1,32 @@
+#rotate the layers of subplot in interval 0f 2 seconds
 import numpy as np
 import matplotlib.pyplot as plt
 
 x=np.array([35,25,25,15])
-y=np.array([2,5,4,11])
+y=np.array([2,5,4,7])
+plt.fill_between(x,y,alpha=0.5,color='lightblue')
 
-plt.subplot(3,1,1)
+plt.subplot(2,2,1)
 plt.bar(y,x,color='purple')
 plt.grid()
 plt.title('Bar Chart')
 plt.xlabel('X axis')
 plt.ylabel('Y axis')
 
-plt.subplot(3,1,2)
+plt.subplot(2,2,2)
 plt.scatter(y,x,color='orange')
 plt.title('Scatter Plot')
 plt.xlabel('X axis')
 plt.ylabel('Y axis')
 plt.tight_layout()
 
-plt.subplot(3,1,3)
-x=np.array([35,25,25,15])
-y=['Apple','Banana','Cherry','Date']
+plt.subplot(2,2,3)
 c=['#ff9999','#66b3ff','#99ff99','#ffcc99']
 ex=(0.1,0.2,0.3,0)
 plt.pie(x,labels=y,autopct='%1.1f%%',colors=c,explode=ex,shadow=True)
 plt.legend(title="Fruits",loc="best")
+
+plt.subplot(2,2,4)
+plt.plot(x,linestyle='dotted',marker='o',color='red')
+plt.plot(y,linestyle='dashed',c='#4CAF50',marker='x',linewidth=10)
 plt.show()
